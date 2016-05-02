5bf5dc57eea7e558a22403f644d55a9ffb952a38
Robyn Smith <rtasmith05@gmail.com>
2016-05-01 21:11:12 -0400

    Colision detection implimented

--- main.js
+++ main.js
@@ -3,16 +3,18 @@ var playerTurn = "x";
 $(document).ready(function() {
   $(".box-content").click(function() {
     var buttonContent = $(this).attr("value");
-    console.log(buttonContent);
 
-    if (playerTurn === "x") {
+    console.log($(this).text().length);
+
+    if ( $(this).text().lenght === 0 ) {
+      //do nothing
+    } 
+    else if (playerTurn === "x") {
       $(this).html("x");
       playerTurn = "o";
-
     } else if (playerTurn === "o") {
-      $(this).html("o");
+      $(this).html("x");
       playerTurn = "x";
     }
-
   });
 });
